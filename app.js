f485d9c83013982f727b9a4a8bdd36bebce33259
idcreationsc <mecado.maretinez28@gmail.com>
2026-09-10 23:34:35 -0400

    Corrige la carga global de la aplicación

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 // Carga estable de la tienda ID Creation SC
-fetch("https://raw.githubusercontent.com/idcreationsc/id-creation-sc-store/d6f7f0752ac931dfe9b4c11f7ae727e325143f49/app.js")
-  .then(r => {
-    if (!r.ok) throw new Error("No se pudo cargar la aplicación");
-    return r.text();
-  })
-  .then(code => (0, eval)(code))
-  .catch(err => console.error("ID Creation SC:", err));
+// Inserta la aplicación principal como script clásico para que sus variables globales
+// estén disponibles para supabase-bridge.js.
+document.write('<script src="https://raw.githubusercontent.com/idcreationsc/id-creation-sc-store/d6f7f0752ac931dfe9b4c11f7ae727e325143f49/app.js"><\/script>');
